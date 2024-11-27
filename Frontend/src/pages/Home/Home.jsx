6a078fe25f588ec1bbba4d4027220b2e32bc2f57
th4nh-phat09 <thanphat354@gmail.com>
2024-11-27 22:48:32 +0700

    Create CartPage Component

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import Banner from './Banner'
-import TopSellers from './TopSellers'
-import Recommend from './Recommend'
-import News from './News'
+import React from "react";
+import Banner from "./Banner";
+import TopSellers from "./TopSellers";
+import Recommend from "./Recommend";
+import News from "./News";
+import Footer from "../../components/Footer";
 const Home = () => {
   return (
     <>
-     <Banner />
-     <TopSellers />
-     <Recommend />
-     <News />
+      <Banner />
+      <TopSellers />
+      <Recommend />
+      <News />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- Frontend/src/routes/routers.jsx
+++ Frontend/src/routes/routers.jsx
@@ -3,16 +3,18 @@ import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/Book/CartPage";
 const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "order", element: <h1>Order</h1> },
-        { path: "profile", element: <h1>Profile</h1>},
-        { path: "login", element: <Login />},
-        { path: "register", element: <Register />},
+        { path: "/order", element: <h1>Order</h1> },
+        { path: "/profile", element: <h1>Profile</h1>},
+        { path: "/login", element: <Login />},
+        { path: "/register", element: <Register />},
+        { path: "/cart", element: <CartPage />},
       ]
     },
   ]);
