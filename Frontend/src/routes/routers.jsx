79832f026a0f24756b12582876efa36ccc8eacd2
th4nh-phat09 <thanphat354@gmail.com>
2024-11-30 20:57:28 +0700

    Create Checkout Page

--- Frontend/src/routes/routers.jsx
+++ Frontend/src/routes/routers.jsx
@@ -1,22 +1,24 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/Book/CartPage";
+import CheckoutPage from "../pages/Book/CheckoutPage";
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "/order", element: <h1>Order</h1> },
-        { path: "/profile", element: <h1>Profile</h1>},
-        { path: "/login", element: <Login />},
-        { path: "/register", element: <Register />},
-        { path: "/cart", element: <CartPage />},
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/order", element: <h1>Order</h1> },
+      { path: "/profile", element: <h1>Profile</h1> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/cart", element: <CartPage /> },
+      { path: "/checkout", element: <CheckoutPage /> },
+    ],
+  },
+]);
 
-export default router
+export default router;
